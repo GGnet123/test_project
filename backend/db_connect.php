621d198001df24d0a1d341063bb0e4454b208250
Галым Жарысов <g.zharysov@affstar.com>
2022-05-07 02:47:18 +0600

    Revert "first commit"
    This reverts commit 213b574a81f888e6465906cba211b1df64900ba7.

--- backend/db_connect.php
+++
@@ -1,3 +0,0 @@
-<?php
-$conn_string = "host=postgres dbname=".$_ENV['POSTGRES_DB']." user=".$_ENV['POSTGRES_USER']." password=" . $_ENV['POSTGRES_PASSWORD'];
-return pg_connect($conn_string);
--- index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require __DIR__ . "/backend/router.php";
-require_once realpath(__DIR__ . '/vendor/autoload.php');
-
-$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
